5db451f385cc8eec323d870f639bb728d482b694
perampokgoogle <localhost367@gmail.com>
2017-04-27 23:09:27 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Jagocoding</title>  
+    </head>
+    <body><br>        <div id="container">
+        <div id="header">
+            Header
+        </div>
+        <!-- #header END -->
